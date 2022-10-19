fadfaa094968e17b5054c0e0802fd89066b3cea6
想养猫的CXY <zhaozongwen@gmail.com>
2022-10-19 20:18:47 +0800

    Add files via upload

---
+++ 二分查找.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int search_binary(vector<int>v, int value)
+{
+	int low = 0;
+	int high = v.size() - 1;
+	int mid = (low + high) / 2;
+	while (low <= high)
+	{
+		
+		if (v[mid] == value)
+		{
+			return mid;
+		}
+		else if (value < v[mid])
+		{
+			high = mid-1;
+		}
+		else
+		{
+			low = mid + 1;
+		}
+		mid= (low + high) / 2;
+	}
+	return -1;
+}
+
+int main()
+{
+	vector<int>v{ 8,11,19,23,27,33,45,55,67,98 };
+	cout << search_binary(v, 10);
+}
+
