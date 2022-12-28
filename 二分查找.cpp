67599db2116cef467f7f791877ae390a78d67e46
想养猫的CXY <zhaozongwen@gmail.com>
2022-12-28 12:48:47 +0800

    Update 二分查找.cpp

--- 二分查找.cpp
+++ 二分查找.cpp
@@ -1,35 +1,46 @@
-#include<iostream>
-#include<vector>
+#include <iostream>
 using namespace std;
 
-int search_binary(vector<int>v, int value)
-{
-	int low = 0;
-	int high = v.size() - 1;
-	int mid = (low + high) / 2;
-	while (low <= high)
-	{
-		
-		if (v[mid] == value)
-		{
-			return mid;
-		}
-		else if (value < v[mid])
-		{
-			high = mid-1;
-		}
+int BinarySearch(int *array, int aSize, int key) {
+	if ( array == NULL || aSize == 0 )
+		return -1;
+	int left = 0;
+	int right = aSize - 1;
+	int mid = 0;
+	while ( left <= right ) {
+		mid = (left + right ) / 2;
+		if ( array[mid] < key)
+			left = mid + 1;
+		else if ( array[mid] > key )
+			right = mid - 1;
 		else
-		{
-			low = mid + 1;
-		}
-		mid= (low + high) / 2;
+			return mid;
 	}
 	return -1;
-}
+}//非递归
 
-int main()
-{
-	vector<int>v{ 8,11,19,23,27,33,45,55,67,98 };
-	cout << search_binary(v, 10);
-}
+int BinarySearchRecursive(int *array, int left, int right, int key) {
+	if (left > right) {
+		return -1;
+	}
+	int middle = (left + right) / 2;
+	if (array[middle] == key) {
+		return middle;
+	} else if (array[middle] < key) {
+		return BinarySearchRecursive(array, middle + 1, right, key);
+	} else
+		return BinarySearchRecursive(array, left, middle - 1, key);
+}//递归
 
+int main() {
+	int array[8];
+	for (int i = 0; i < 8; i++) {
+		cin >> array[i];
+	}
+	int x;
+	cin >> x;
+	cout << "No recursive:" << endl;
+	cout << "position:" << BinarySearch(array, 8, x) << endl;
+	cout << "recursive:" << endl;
+	cout << "position:" << BinarySearchRecursive(array, 0, 7, x) << endl;
+}
